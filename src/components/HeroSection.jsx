943179d2bcf3b9baaaf8eff5966f5df7a9b771f7
Bryan <bryansanudo95@gmail.com>
2023-04-07 13:02:00 -0500

    herovideo local

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -24,7 +24,7 @@ const HeroSection = ({ isMenuShown }) => {
       <video
         ref={videoRef}
         autoPlay
-        src="https://situla.bitbit.net/filebin/25304676e852aa5ecc5b21b6f4d6cb52d78afcfdf6cf61ea028cb0299be74a36/707f334cabdc410adddac567a42547bf285b0524503e3eccf8ec6f23e1e5afd8?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=HZXB1J7T0UN34UN512IW%2F20230407%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230407T172153Z&X-Amz-Expires=30&X-Amz-SignedHeaders=host&response-cache-control=max-age%3D30&response-content-disposition=filename%3D%22videoplayback__1_.webm%22&response-content-type=video%2Fwebm&X-Amz-Signature=d49a9baa7f0ec1e7e2bebdb1697f115780f929220d57cbacf236e6c5d4cfe314"
+        src="/adidas.mp4"
         loop
         type="video/mp4"
         playsInline
